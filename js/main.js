90c8739c2495ca735bfb9ef37a3d5e978cc3dbfa
emad <emadhibishy@gmail.com>
2021-01-18 23:48:24 +0200

    adding animations

---
+++ js/main.js
@@ -0,0 +1,65 @@
+
+$(function(){
+    // scroll to element
+    function scrlToEl(element){
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(element).offset().top +40
+        }, 1500);
+    }
+    // adding class
+    function addActive(element){
+        $(element).addClass('active').children('a').children('span').css({color:'rgb(67 , 61 , 154)'});
+        $(element).children('a').children('i').css({color:'rgb(67 , 61 , 154)'});
+    }
+    // remove active
+    function removeActive(){
+        $('.aside ul li').removeClass('active').children('a').children('span').css({color:'#fff'});
+        $('.aside ul li').children('a').children('i').css({color:'#fff'});
+    }
+
+    // aside active and showing it's target div
+    $('.aside ul li').on('click',function(){
+        const target        = $(this).children('a').data('target');
+        removeActive();
+        addActive(this);
+        scrlToEl(target);
+    });
+
+    // add active class to portfolio projects headers and showing it's target div
+    $('#portfolio ul li').on('click',function(){
+        const target = $(this).data('target');
+        $(this).addClass('active').siblings().removeClass('active');
+        $('.portfolio__projects').fadeOut();
+        $(target).fadeIn(1000);
+
+    });
+    // showing project details on hover
+    $('#portfolio .project').on({
+        mouseenter: function(){$(this).children('.overlay').show()},
+        mouseleave: function(){$(this).children('.overlay').hide()}
+    });
+
+    // add active class to aside element when scroll to its target div
+    let mains = $('.main');
+    $(window).scroll(function(){
+        let scrlVal = $(window).scrollTop() ;
+        for(i = 0; i < mains.length; i++){
+            let target = $(mains[i]).attr('id');
+            if (i === 4){
+                if(scrlVal >= $(mains[4]).offset().top){
+                    removeActive();
+                    addActive('#aside-'+target);
+                }
+                continue;
+            }
+            else{
+                if(scrlVal >= $(mains[i]).offset().top && scrlVal < $(mains[i+1]).offset().top ){
+                    removeActive();
+                    addActive('#aside-'+target);
+                }
+            }
+        }
+    });
+    
+    
+});
